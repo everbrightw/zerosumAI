5a6383ce782ad3c549cac7c90905f081b07fc859
everbrightw <yusencs@163.com>
2017-10-26 22:54:14 -0500

    bug fixed

--- src/Board.java
+++ src/Board.java
@@ -14,23 +14,34 @@ public class Board{
             }
         }
 
-        for (int i = 0; i < 7 ; i++) {
+
+        for (int i = 0; i < 8 ; i++) {
             for (int j = 0; j <2 ; j++) {
                 board[i][j] = new Node(i, j, BLACK);
             }
         }
 
-        for (int i = 0; i < 7 ; i++) {
+        for (int i = 0; i < 8 ; i++) {
             for (int j = 6; j < 8 ; j++) {
                 board[i][j] = new Node(i, j, WHITE);
             }
         }
+
+       
+
+
     }
 
     public static void print(){
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
+                if(board[j][i] != null){
                 System.out.print(board[j][i].color.charAt(0) + " ");
+
+                }
+                else{
+                    System.out.print("  ");
+                }
             }
             System.out.println();
         }
--- src/Node.java
+++ src/Node.java
@@ -1,35 +1,15 @@
 
 // each instance of node represent a chessman
 public class Node {
-    public static Node[][] board = new Node[8][8];
-
-    public static void setBoard(){
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
-                board[i][j] = null;
-            }
-        }
-
-        for (int i = 0; i < 7 ; i++) {
-            for (int j = 0; j <2 ; j++) {
-                board[i][j] = new Node(i, j, BLACK);
-            }
-        }
-
-        for (int i = 0; i < 7 ; i++) {
-            for (int j = 6; j < 8 ; j++) {
-                board[i][j] = new Node(i, j, WHITE);
-            }
-        }
-    }
+ 
     public int x;
 
     public int y;
 
-    public static String color;
+    public String color;
 
     public boolean hasChess(int i,int j){
-        return !(board[i][j] == null);
+        return !(Board.board[i][j] == null);
     }
 
     public static final String WHITE = "white";
@@ -39,48 +19,48 @@ public class Node {
         //default constructor
     }
 
-    public Node(int x, int y, String color) {
+    public Node(int x, int y, String c) {
         this.x = x;
         this.y = y;
-        this.color = color;
+        this.color = c;
     }
 
     // chess moving
     public void moveUp() {
         int newY = y -1;
         Node newNode = new Node(x, newY, color);
-        board[x][y] = null;
-        board[x][newY] = newNode;
+        Board.board[x][y] = null;
+        Board.board[x][newY] = newNode;
     }
 
     public void moveDown() {
         int newY = y + 1;
         Node newNode = new Node(x, newY, color);
-        board[x][y] = null;
-        board[x][newY] = newNode;
+        Board.board[x][y] = null;
+        Board.board[x][newY] = newNode;
     }
     public void moveUpRight() {
         int newY = y -1;
         int newX = x+1;
         Node newNode = new Node(newX, newY, color);
-        board[x][y] = null;
-        board[newX][newY] = newNode;
+        Board.board[x][y] = null;
+        Board.board[newX][newY] = newNode;
     }
 
     public void moveUpLeft() {
         int newY = y - 1;
         int newX = x - 1;
         Node newNode = new Node(newX, newY, color);
-        board[x][y] = null;
-        board[newX][newY] = newNode;
+        Board.board[x][y] = null;
+        Board.board[newX][newY] = newNode;
     }
 
     public void moveDownRight() {
         int newY = y + 1;
         int newX = x + 1;
         Node newNode = new Node(newX, newY, color);
-        board[x][y] = null;
-        board[newX][newY] = newNode;
+        Board.board[x][y] = null;
+        Board.board[newX][newY] = newNode;
 
     }
 }
